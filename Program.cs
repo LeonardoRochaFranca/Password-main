fec537eb40a4cad71c5f35eec9444c4beecda652
LeonardoRochaFranca <franca.leonardorocha@gmail.com>
2025-03-07 15:29:42 -0300

    set main class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Password_Main
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}
